95b2fef920c87e0269b2e78685d9aa89ecb7c682
egorvodopyanov <egorvodopyanov@gmail.com>
2019-08-30 17:11:08 +0300

    enzyme test

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,42 +1,42 @@
+/**
+ * @jest-environment jsdom
+ */
 import { View } from "react-native";
-import renderer from "react-test-renderer";
-import { mount, shallow } from "enzyme";
+import { mount } from "enzyme";
 import React from "react";
 
 import { PortalEnter, PortalExit, PortalProvider } from "../lib/index";
 
-test("renders correctly", () => {
+test("mounts correctly", () => {
+  const ToPort = () => <View test-name="to-port" />;
   const App = (
     <PortalProvider>
-      <View style={{ backgroundColor: "red" }}>
+      <View test-name="top">
         <PortalEnter name="example">
-          <View style={{ marginTop: 1 }} />
-        </PortalEnter>
-        <PortalEnter name="example-2">
-          <View style={{ marginTop: 2 }} />
+          <ToPort />
         </PortalEnter>
       </View>
       <PortalExit />
     </PortalProvider>
   );
-  const tree = renderer.create(App).toJSON();
-  expect(tree).toMatchSnapshot();
+  const wrapper = mount(App);
+
+  expect(wrapper).toMatchSnapshot();
 });
 
-test("mounts correctly", () => {
+test("PortalExit contains ToPort component", () => {
+  const ToPort = () => <View test-name="to-port" />;
   const App = (
     <PortalProvider>
-      <View style={{ backgroundColor: "red" }}>
+      <View test-name="top">
         <PortalEnter name="example">
-          <View style={{ marginTop: 1 }} />
-        </PortalEnter>
-        <PortalEnter name="example-2">
-          <View style={{ marginTop: 2 }} />
+          <ToPort />
         </PortalEnter>
       </View>
       <PortalExit />
     </PortalProvider>
   );
-  const wrapper = shallow(App);
-  expect(wrapper.dive()).toMatchSnapshot();
+  const wrapper = mount(App);
+
+  expect(wrapper.find(PortalExit).find(ToPort)).toMatchSnapshot();
 });
--- tests/setup.js
+++ tests/setup.js
@@ -1,4 +1,27 @@
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
+/**
+ * Set up DOM in node.js environment for Enzyme to mount to
+ */
+const { JSDOM } = require('jsdom');
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
+function copyProps(src, target) {
+  Object.defineProperties(target, {
+    ...Object.getOwnPropertyDescriptors(src),
+    ...Object.getOwnPropertyDescriptors(target),
+  });
+}
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+copyProps(window, global);
+
+
 Enzyme.configure({ adapter: new Adapter() });
